f87b835b013c5db765fb2a8f8ffa5671c933aaf6
Mikayla Fischler <mikayla@ky8.io>
2016-09-27 09:48:10 -0400

    use specific strip in test code

--- test.ino
+++ test.ino
@@ -3,17 +3,17 @@
 // Test LEDs using the code from strandtest.ino
 void test(Adafruit_NeoPixel strip) {
   // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  colorWipe(strip, strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip, strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip, strip.Color(0, 0, 255), 50); // Blue
   
   // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127, 0, 0), 50);     // Red
-  theaterChase(strip.Color(0, 0, 127), 50);     // Blue
+  theaterChase(strip, strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip, strip.Color(127, 0, 0), 50);     // Red
+  theaterChase(strip, strip.Color(0, 0, 127), 50);     // Blue
 
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
+  rainbow(strip, 20);
+  rainbowCycle(strip, 20);
+  theaterChaseRainbow(strip, 50);
 }
 
