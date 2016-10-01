c6ac6d6b70cf48665c23dfaa67988cf29d50ac6e
Mikayla Fischler <mikayla@ky8.io>
2016-10-01 16:24:47 -0400

    purple fade

--- dorm-leds.ino
+++ dorm-leds.ino
@@ -33,25 +33,41 @@ void setup() {
   // initialize LED strips and set them to off
   init_strips();
 }
-
+  
 // the loop function runs over and over again forever
 void loop() {
   // test LEDs
   //test(strand);
 
   for(int i = 0; i < desk1.numPixels(); i++){
-    desk1.setPixelColor(i, desk1.Color(100,100,100));
-    delay(20);
+    desk1.setPixelColor(i, desk1.Color(100,50,150));
+    //delay(20);
     desk1.show();
   }
 
   for(int i = 0; i < desk2.numPixels(); i++){
-    desk2.setPixelColor(i, desk2.Color(100,100,100));
-    delay(20);
+    desk2.setPixelColor(i, desk2.Color(100,50,150));
+    //delay(20);
     desk2.show();
   }
 
-  fulltest();
+  for(int i = 0; i <= 150; i++){
+    for(int x = 0; x < window_generic.numPixels(); x++){
+      setAllWindowPixelColor(x, window_generic.Color((int)(((float)i / 150.0) * 100), 0, i));
+    }
+    delay(10);
+    showAllWindowStrips();
+  }
+
+  for(int i = 150; i >= 0; i--){
+    for(int x = 0; x < window_generic.numPixels(); x++){
+      setAllWindowPixelColor(x, window_generic.Color((int)(((float)i / 150.0) * 100), 0, i));
+    }
+    delay(10);
+    showAllWindowStrips();
+  }
+
+  //fulltest();
 
   //led_main_loop();
   //ctrl_main_loop();
