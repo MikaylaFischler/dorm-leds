ea794e7a3f2a8fd7c9403d81d24ec4486b14a7b4
Mikayla Fischler <mikayla@ky8.io>
2016-10-01 01:27:29 -0400

    file renames, set up basic functional code for all systems

--- init.ino
+++ init.ino
@@ -1,18 +1,15 @@
 /* Initialization Code for the Dorm LED System */
 
 void set_pin_modes() {
-  pinMode(STRIP_PIN_W1, OUTPUT);
-  pinMode(STRIP_PIN_W2, OUTPUT);
-  pinMode(STRIP_PIN_W3, OUTPUT);
-  pinMode(STRIP_PIN_DT, OUTPUT);
-  pinMode(STRIP_PIN_DB, OUTPUT);
+ //set pin modes here
 }
 
 void init_strips() {
   // testing
-  strand.begin();
-  strand.show();
-  /*
+  //strand.begin();
+  //strand.show();
+  
+  
   // Window Strips
   window1.begin();
   window1.show();
@@ -28,6 +25,6 @@ void init_strips() {
   desk1.show();
   
   desk2.begin();
-  desk2.show();*/
+  desk2.show();
 }
 
---
+++ led_subsystem.ino
@@ -0,0 +1,10 @@
+/*
+  Dorm LED Project: led_subsystem.ino
+  This file manages timing and execution of LED commands.
+
+  Created by: Michael Fischler
+  9/30/2016 @ WPI
+*/
+
+
+
