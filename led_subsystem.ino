2c7f863b45094ab87b56869175c2e779542a425c
Michael Fischler <micron1314@gmail.com>
2016-10-22 17:09:19 -0400

    added timing code

--- led_subsystem.ino
+++ led_subsystem.ino
@@ -6,7 +6,22 @@
   9/30/2016 @ WPI
 */
 
+unsigned long int dT = 0;
+
+ThreadHandler led_thread_handler;
+
 void led_main_loop(){
-  
+  // set change in time
+  cur_time = millis();
+  dT = cur_time - prev_time;
+
+  // tell each thread the time change
+  led_thread_handler.updateTimeAccumulated(dT);
+
+  // execute commands that it is time to execute
+  led_thread_handler.executeTick();
+
+  // save this time as previous time
+  prev_time = millis();
 }
 
