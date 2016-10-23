9b355e5545e85cd9181f4f0951733ef87bd1cca0
Mikayla Fischler <mikayla@ky8.io>
2016-10-23 02:46:01 -0400

    files for commands

---
+++ def/led_commands.cpp
@@ -0,0 +1,9 @@
+/*
+  Dorm LED Project: led_commands.cpp
+  This file contains the Command definitions for all command functions.
+
+  These are *ready* to be executed using the multithreaded system.
+
+  Created by: Michael Fischler
+  10/22/2016 @ WPI
+*/
--- def/led_window_anim_cmds.cpp
+++ def/led_window_anim_cmds.cpp
@@ -1,5 +1,5 @@
 /*
-  Dorm LED Project: led_window_anim_cmds.ino
+  Dorm LED Project: led_window_anim_cmds.cpp
   This file contains the window LED animation commands.
 
   These are meant to be executed using the multithreaded system.
@@ -11,20 +11,19 @@
 /*
   Template for a Command
   std::vector<var> (*cmd)(std::vector<var>)
-  
+
   std::vector<var> cmd(std::vector<var> var_stack){
     // set variables
     int i = var_stack.at(0).value;
     int t = var_stack.at(1).value;
-  
+
     // run command code
     ...
-  
+
     // update variables
     var_stack.at(0).value = i;
     var_stack.at(1).value = t;
-  
+
     return var_stack;
   }
 */
-
