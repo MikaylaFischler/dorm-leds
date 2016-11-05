92c63143a56b9faaa7cdcd603159f93dfa2e2a45
Mikayla Fischler <mikayla@ky8.io>
2016-11-05 01:13:36 -0400

    updated header file

--- lib/LocalStack.h
+++ lib/LocalStack.h
@@ -9,41 +9,42 @@ class LocalStack {
 private:
     std::vector<void*> stack;
 public:
-  LocalStack(void*[] init);
-  ~LocalStack();
+    LocalStack();
+    LocalStack(void*[] init, int numElements);
+    ~LocalStack();
 
-  // untyped element manipulation
-  void push(void* element);
-  void* get(unsigned int i);
+    // untyped element manipulation
+    void push(void* element);
+    void* get(unsigned int i);
 
-  // get raw stack
-  std::vector<void*> getStack();
+    // get raw stack
+    std::vector<void*> getStack();
 
-  /* ~~~ Typed Getters ~~~ */
-  // bool
-  bool getBool(unsigned int i);
+    /* ~~~ Typed Getters ~~~ */
+    // bool
+    bool getBool(unsigned int i);
 
-  // integers
-  int getInt(unsigned int i);
-  short int getShortInt(unsigned int i);
-  long int getLongInt(unsigned int i);
-  unsigned int getUInt(unsigned int i);
-  unsigned short int getUnsignShortInt(unsigned int i);
-  unsigned long int getUnsignLongInt(unsigned int i);
+    // integers
+    int getInt(unsigned int i);
+    short int getShortInt(unsigned int i);
+    long int getLongInt(unsigned int i);
+    unsigned int getUInt(unsigned int i);
+    unsigned short int getUnsignShortInt(unsigned int i);
+    unsigned long int getUnsignLongInt(unsigned int i);
 
-  // floating point numbers
-  float getFloat(unsigned int i);
-  double getDouble(unsigned int i);
+    // floating point numbers
+    float getFloat(unsigned int i);
+    double getDouble(unsigned int i);
 
-  // text
-  char getChar(unsigned int i);
-  unsigned char getUnsignChar(unsigned int i);
-  String getString(unsigned int i);
+    // text
+    char getChar(unsigned int i);
+    unsigned char getUnsignChar(unsigned int i);
+    String getString(unsigned int i);
 
-  // some common arrays
-  bool[] getBoolArray(unsigned int i);
-  int[] getIntArray(unsigned int i);
-  short int[] getShortIntArray(unsigned int i);
+    // some common arrays
+    bool[] getBoolArray(unsigned int i);
+    int[] getIntArray(unsigned int i);
+    short int[] getShortIntArray(unsigned int i);
 };
 
 #endif
