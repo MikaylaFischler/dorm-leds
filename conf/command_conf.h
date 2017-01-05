e9fb3586f43405dc2f7e98f98c6ce531487d58a3
Michael1516 <micron1314@gmail.com>
2017-01-05 18:01:37 -0500

    command configuration file

---
+++ conf/command_conf.h
@@ -0,0 +1,12 @@
+// command configurations
+const short BOTTOM_DESK[1] = {DESK1};
+const short TOP_DESK[1]    = {DESK2};
+const short FULL_DESK[2]   = {DESK1, DESK2};
+
+const short win1[1]   = {WINDOW1};
+const short win2[1]   = {WINDOW2};
+const short win3[1]   = {WINDOW3};
+const short win12[2]  = {WINDOW1, WINDOW2};
+const short win13[2]  = {WINDOW1, WINDOW3};
+const short win23[2]  = {WINDOW2, WINDOW3};
+const short allwin[3] = {WINDOW1, WINDOW2, WINDOW3};
