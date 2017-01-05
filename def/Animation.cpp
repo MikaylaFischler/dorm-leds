20b511f322e64615347328e6fd4a953e298fecc0
Mikayla Fischler <mikayla@ky8.io>
2017-01-05 18:13:34 -0500

    updated commands and removed executable refs

--- def/Animation.cpp
+++ def/Animation.cpp
@@ -4,7 +4,7 @@
 Animation::Animation () { init(); }
 
 // <<destructor>>
-Animation::~Animation () {}
+Animation::~Animation () { delete this->command; }
 
 // get name
 String Animation::getName () { return this->name; }
@@ -14,6 +14,9 @@ int Animation::getNumStrips () { return this->num_strips; }
 
 // get specific strip dependencies
 std::vector<int> Animation::getDependencies () { return this->strips; }
+
+// get the command created by the animation
+Command* getCommand () { return this->command; }
 
 // get update rate
 int Animation::getUpdateRate () { return this->updateRate; }
