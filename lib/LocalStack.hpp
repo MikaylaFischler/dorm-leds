6f11396f485936089edbc4d1783dd1daa20e204b
Michael1516 <micron1314@gmail.com>
2017-01-06 17:00:32 -0500

    new memory system

--- lib/LocalStack.hpp
+++ lib/LocalStack.hpp
@@ -3,52 +3,51 @@
 
 #include <StandardCplusplus.h>
 #include <vector>
-#include <Arduino.h>
 
 class LocalStack {
     private:
-        std::vector<void*> stack;
+        std::vector<MemObj> stack;
     public:
-        LocalStack();
-        LocalStack(void* datum);
-        LocalStack(void* init[], int numElements);
-        ~LocalStack();
+        LocalStack ();
+        LocalStack (MemObj mem);
+        LocalStack (MemObj stack[], int numElements);
+        ~LocalStack ();
 
         // untyped element manipulation
-        void push(void* element);
-        void* get(unsigned int i);
-        void update(unsigned int i, void* val);
-        void drop(unsigned int i);
+        void push (MemObj element);
+        MemObj get (unsigned int i);
+        void update (unsigned int i, MemObj val);
+        /* DEPRECATED void drop (unsigned int i); */
 
         // get raw stack
-        std::vector<void*> getStack();
+        std::vector<MemObj> getStack();
 
         /* ~~~ Typed Getters ~~~ */
         // bool
-        bool getBool(unsigned int i);
+        bool getBool (unsigned int i);
 
         // integers
-        int getInt(unsigned int i);
-        short int getShortInt(unsigned int i);
-        long int getLongInt(unsigned int i);
-        unsigned int getUnsignInt(unsigned int i);
-        unsigned short int getUnsignShortInt(unsigned int i);
-        unsigned long int getUnsignLongInt(unsigned int i);
+        int getInt (unsigned int i);
+        short int getShortInt (unsigned int i);
+        long int getLongInt (unsigned int i);
+        unsigned int getUnsignInt (unsigned int i);
+        unsigned short int getUnsignShortInt (unsigned int i);
+        unsigned long int getUnsignLongInt (unsigned int i);
 
         // floating point numbers
-        float getFloat(unsigned int i);
-        double getDouble(unsigned int i);
-        long double getLongDouble(unsigned int i);
+        float getFloat (unsigned int i);
+        double getDouble (unsigned int i);
+        long double getLongDouble (unsigned int i);
 
         // text
-        char getChar(unsigned int i);
-        unsigned char getUnsignChar(unsigned int i);
-        String getString(unsigned int i);
+        char getChar (unsigned int i);
+        unsigned char getUnsignChar (unsigned int i);
+        String getString (unsigned int i);
 
         // some common arrays
-        bool * getBoolArray(unsigned int i);
-        int * getIntArray(unsigned int i);
-        short int * getShortIntArray(unsigned int i);
+        bool * getBoolArray (unsigned int i);
+        int * getIntArray (unsigned int i);
+        short int * getShortIntArray (unsigned int i);
 };
 
 #endif
---
+++ lib/MemObj.cpp
@@ -0,0 +1,21 @@
+#include "MemObj.hpp"
+
+// <<constructor>>
+MemObj::MemObj (void* var) { this->var = var; }
+
+// <<destructor>>
+// WARNING: YOU MUST CALL DESTROY WHEN THIS OBJECT IS DONE BEING USED [WITH KNOWLEDGE OF THE TYPE]
+// OTHERWISE THE `var` WILL BE A MEMORY LEAK
+MemObj::~MemObj () {}
+
+// get the data
+mem_type& MemObj::get () { return (mem_type) &data; }
+
+// update the data
+void MemObj::set (mem_type* new_data) {
+    delete (mem_type*) data;
+    this->data = new_data;
+}
+
+// free the memory
+void MemObj::destroy () { delete (mem_type*) data; }
