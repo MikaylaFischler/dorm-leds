e064fe03b9d5b4f7db23aa83a579d672ba5a09fa
Mikayla Fischler <mikayla@ky8.io>
2017-01-06 17:00:32 -0500

    new memory system

--- lib/LocalStack.cpp
+++ lib/LocalStack.cpp
@@ -1,136 +1,76 @@
-#include "LocalStack.h"
+#include "LocalStack.hpp"
 
 // <<constructors>>
-LocalStack::LocalStack(){}
+LocalStack::LocalStack () {}
 
-LocalStack::LocalStack(void* datum){
-  this->stack.push_back(datum);
+LocalStack::LocalStack (MemObj datum) {
+    this->stack.push_back(datum);
 }
 
-LocalStack::LocalStack(void* stack[], int numElements){
-  for(int i = 0; i < numElements; i++){
-    this->stack.push_back(stack[i]);
-  }
+LocalStack::LocalStack (MemObj stack[], int numElements) {
+    for (int i = 0; i < numElements; i++) {
+        this->stack.push_back(stack[i]);
+    }
 }
 
 // <<destructor>>
-LocalStack::~LocalStack(){}
+LocalStack::~LocalStack () {}
 
 // untyped methods and manipulations
-void LocalStack::push(void* datum){
-  this->stack.push_back(datum);
+void LocalStack::push (void* datum) {
+    this->stack.push_back(datum);
 }
 
-void* LocalStack::get(unsigned int i){
-  return this->stack.at(i);
+void* LocalStack::get (unsigned int i) {
+    return this->stack.at(i);
 }
 
-void LocalStack::update(unsigned int i, void* val){
-  this->stack.at(i) = val;
+void LocalStack::update (unsigned int i, void* val) {
+    this->stack.at(i) = val;
 }
 
-void LocalStack::drop(unsigned int i){
-  this->stack.erase(this->stack.begin() + i);
+/**
+No Longer Allowed: Would cause a memory leak
+void LocalStack::drop (unsigned int i) {
+    this->stack.erase(this->stack.begin() + i);
 }
+**/
 
-// typed get functions
+/* ~~~ Typed Get Functions ~~~ */
+
 // boolean
-bool LocalStack::getBool(unsigned int i){
-  void* stack_ptr = this->get(i);
-	bool* ptr = (bool*) stack_ptr;
-	return *ptr;
-}
+bool LocalStack::getBool (unsigned int i) { return this->get(i).get<bool>(); }
 
 // integers
-int LocalStack::getInt(unsigned int i){
-  void* stack_ptr = this->get(i);
-  int* ptr = (int*) stack_ptr;
-  return *ptr;
-}
+int LocalStack::getInt (unsigned int i) { return this->get(i).get<int>(); }
 
-short int LocalStack::getShortInt(unsigned int i){
-  void* stack_ptr = this->get(i);
-  short int* ptr = (short int*) stack_ptr;
-  return *ptr;
-}
+short int LocalStack::getShortInt (unsigned int i) { return this->get(i).get<short int>(); }
 
-long int LocalStack::getLongInt(unsigned int i){
-  void* stack_ptr = this->get(i);
-  long int* ptr = (long int*) stack_ptr;
-  return *ptr;
-}
+long int LocalStack::getLongInt (unsigned int i) { return this->get(i).get<long int>(); }
 
-unsigned int LocalStack::getUnsignInt(unsigned int i){
-  void* stack_ptr = this->get(i);
-  unsigned int* ptr = (unsigned int*) stack_ptr;
-  return *ptr;
-}
+unsigned int LocalStack::getUnsignInt (unsigned int i) { return this->get(i).get<unsigned int>(); }
 
-unsigned short int LocalStack::getUnsignShortInt(unsigned int i){
-  void* stack_ptr = this->get(i);
-  unsigned short int* ptr = (unsigned short int*) stack_ptr;
-  return *ptr;
-}
+unsigned short int LocalStack::getUnsignShortInt (unsigned int i) { return this->get(i).get<unsigned short int>(); }
 
-unsigned long int LocalStack::getUnsignLongInt(unsigned int i){
-  void* stack_ptr = this->get(i);
-  unsigned long int* ptr = (unsigned long int*) stack_ptr;
-  return *ptr;
-}
+unsigned long int LocalStack::getUnsignLongInt (unsigned int i) { return this->get(i).get<unsigned long int>(); }
 
 // floating point numbers
-float LocalStack::getFloat(unsigned int i){
-  void* stack_ptr = this->get(i);
-  float* ptr = (float*) stack_ptr;
-  return *ptr;
-}
+float LocalStack::getFloat (unsigned int i) { return this->get(i).get<float>(); }
 
-double LocalStack::getDouble(unsigned int i){
-  void* stack_ptr = this->get(i);
-  double* ptr = (double*) stack_ptr;
-  return *ptr;
-}
+double LocalStack::getDouble (unsigned int i) { return this->get(i).get<double>(); }
 
-long double LocalStack::getLongDouble(unsigned int i){
-  void* stack_ptr = this->get(i);
-  long double* ptr = (long double*) stack_ptr;
-  return *ptr;
-}
+long double LocalStack::getLongDouble (unsigned int i) { return this->get(i).get<long double>(); }
 
 // text
-char LocalStack::getChar(unsigned int i){
-  void* stack_ptr = this->get(i);
-  char* ptr = (char*) stack_ptr;
-  return *ptr;
-}
+char LocalStack::getChar (unsigned int i) { return this->get(i).get<char>(); }
 
-unsigned char LocalStack::getUnsignChar(unsigned int i){
-  void* stack_ptr = this->get(i);
-  unsigned char* ptr = (unsigned char*) stack_ptr;
-  return *ptr;
-}
+unsigned char LocalStack::getUnsignChar (unsigned int i) { return this->get(i).get<unsigned char>(); }
 
-String LocalStack::getString(unsigned int i){
-  void* stack_ptr = this->get(i);
-  String* ptr = (String*) stack_ptr;
-  return *ptr;
-}
+String LocalStack::getString (unsigned int i) { return this->get(i).get<String>(); }
 
 // some common arrays
-bool* LocalStack::getBoolArray(unsigned int i){
-  void* stack_ptr = this->get(i);
-  bool* ptr = (bool*) stack_ptr;
-  return ptr;
-}
+bool* LocalStack::getBoolArray (unsigned int i) { return this->get(i).get<bool*>(); }
 
-int* LocalStack::getIntArray(unsigned int i){
-  void* stack_ptr = this->get(i);
-  int* ptr = (int*) stack_ptr;
-  return ptr;
-}
+int* LocalStack::getIntArray (unsigned int i) { return this->get(i).get<int*>(); }
 
-short int* LocalStack::getShortIntArray(unsigned int i){
-  void* stack_ptr = this->get(i);
-  short int* ptr = (short int*) stack_ptr;
-  return ptr;
-}
+short int* LocalStack::getShortIntArray (unsigned int i) { return this->get(i).get<short int*>(); }
