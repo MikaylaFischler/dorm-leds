f8e4427e89f2ae2f00fd650a2e9a7bdd565a2ca7
Michael1516 <micron1314@gmail.com>
2017-01-06 18:35:42 -0500

    simplified localstack

--- lib/LocalStack.cpp
+++ lib/LocalStack.cpp
@@ -3,6 +3,7 @@
 // <<constructors>>
 LocalStack::LocalStack() {}
 
+/*
 LocalStack::LocalStack(MemObj datum) {
     this->stack.push_back(datum);
 }
@@ -12,6 +13,7 @@ LocalStack::LocalStack(MemObj stack[], int numElements) {
         this->stack.push_back(stack[i]);
     }
 }
+*/
 
 // <<destructor>>
 LocalStack::~LocalStack() {}
@@ -35,42 +37,3 @@ void LocalStack::drop(unsigned int i) {
     this->stack.erase(this->stack.begin() + i);
 }
 **/
-
-/* ~~~ Typed Get Functions ~~~ */
-
-// boolean
-bool LocalStack::getBool(unsigned int i) { return this->get(i).get<bool>(); }
-
-// integers
-int LocalStack::getInt(unsigned int i) { return this->get(i).get<int>(); }
-
-short int LocalStack::getShortInt(unsigned int i) { return this->get(i).get<short int>(); }
-
-long int LocalStack::getLongInt(unsigned int i) { return this->get(i).get<long int>(); }
-
-unsigned int LocalStack::getUnsignInt(unsigned int i) { return this->get(i).get<unsigned int>(); }
-
-unsigned short int LocalStack::getUnsignShortInt(unsigned int i) { return this->get(i).get<unsigned short int>(); }
-
-unsigned long int LocalStack::getUnsignLongInt(unsigned int i) { return this->get(i).get<unsigned long int>(); }
-
-// floating point numbers
-float LocalStack::getFloat(unsigned int i) { return this->get(i).get<float>(); }
-
-double LocalStack::getDouble(unsigned int i) { return this->get(i).get<double>(); }
-
-long double LocalStack::getLongDouble(unsigned int i) { return this->get(i).get<long double>(); }
-
-// text
-char LocalStack::getChar(unsigned int i) { return this->get(i).get<char>(); }
-
-unsigned char LocalStack::getUnsignChar(unsigned int i) { return this->get(i).get<unsigned char>(); }
-
-String LocalStack::getString(unsigned int i) { return this->get(i).get<String>(); }
-
-// some common arrays
-bool* LocalStack::getBoolArray(unsigned int i) { return this->get(i).get<bool*>(); }
-
-int* LocalStack::getIntArray(unsigned int i) { return this->get(i).get<int*>(); }
-
-short int* LocalStack::getShortIntArray(unsigned int i) { return this->get(i).get<short int*>(); }
--- lib/LocalStack.hpp
+++ lib/LocalStack.hpp
@@ -9,8 +9,8 @@ class LocalStack {
         std::vector<MemObj> stack;
     public:
         LocalStack();
-        LocalStack(MemObj mem);
-        LocalStack(MemObj stack[], int numElements);
+        //LocalStack(MemObj mem);
+        //LocalStack(MemObj stack[], int numElements);
         ~LocalStack();
 
         // untyped element manipulation
@@ -21,33 +21,6 @@ class LocalStack {
 
         // get raw stack
         std::vector<MemObj> getStack();
-
-        /* ~~~ Typed Getters ~~~ */
-        // bool
-        bool getBool(unsigned int i);
-
-        // integers
-        int getInt(unsigned int i);
-        short int getShortInt(unsigned int i);
-        long int getLongInt(unsigned int i);
-        unsigned int getUnsignInt(unsigned int i);
-        unsigned short int getUnsignShortInt(unsigned int i);
-        unsigned long int getUnsignLongInt(unsigned int i);
-
-        // floating point numbers
-        float getFloat(unsigned int i);
-        double getDouble(unsigned int i);
-        long double getLongDouble(unsigned int i);
-
-        // text
-        char getChar(unsigned int i);
-        unsigned char getUnsignChar(unsigned int i);
-        String getString(unsigned int i);
-
-        // some common arrays
-        bool * getBoolArray(unsigned int i);
-        int * getIntArray(unsigned int i);
-        short int * getShortIntArray(unsigned int i);
 };
 
 #endif
