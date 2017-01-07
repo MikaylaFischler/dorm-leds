2ce4fa6f16b1b94261b19e41642bf99919495181
Michael1516 <micron1314@gmail.com>
2017-01-07 18:10:01 -0500

    holiday animations

---
+++ def/Holiday/Animation_Holiday.hpp
@@ -0,0 +1,35 @@
+#ifndef Animation_Holiday_H
+#define Animation_Holiday_H
+
+#include "../../lib/Animation.hpp"
+
+class Animation_Holiday : public Animation {
+protected:
+    Animation_Holiday() {}
+    ~Animation_Holiday() {}
+
+    void init();
+};
+
+class Animation_Holiday_Halloween_WinAllFade : public Animation_Holiday {
+protected:
+    void init();
+public:
+    Animation_Holiday_Halloween_WinAllFade() {}
+    ~Animation_Holiday_Halloween_WinAllFade() {}
+    void step();
+};
+
+class Animation_Holiday_Halloween_WinAllSparkleFade : public Animation_Holiday {
+private:
+	unsigned long int rand_halloween_color();
+	bool* sparkle_fade(float i, bool inc[]);
+protected:
+	void init();
+public:
+	Animation_Holiday_Halloween_WinAllSparkleFade() {}
+	~Animation_Holiday_Halloween_WinAllSparkleFade() {}
+	void step();
+};
+
+#endif
