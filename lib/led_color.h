58eb66201f6549e5f4f172baa469f84ed291af4b
Mikayla Fischler <mikayla@ky8.io>
2017-01-07 18:10:25 -0500

    updated colors

--- lib/led_color.h
+++ lib/led_color.h
@@ -1,13 +1,16 @@
 // color definitions
 #define COLOR_OFF 0;
 
-#define COLOR_BLACK 0;
-#define COLOR_WHITE 16777215;
+#define COLOR_BLACK 0; // (0, 0, 0)
+#define COLOR_WHITE 16777215; // (255, 255, 255)
 
-#define COLOR_DIM_OFF_WHITE 4926745;
+#define COLOR_DIM_OFF_WHITE 4926745; // (75, 45, 25)
 
-#define COLOR_RED 16711680;
+#define COLOR_LIGHT_SILVER 11120823; // (169, 176, 183)
+
+#define COLOR_MAROON 16717083; // (255, 21, 27)
+#define COLOR_RED 16711680; // (255, 0, 0)
 #define COLOR_ORANGE ;
 #define COLOR_YELLOW ;
-#define COLOR_GREEN ;
-#define COLOR_BLUE ;
+#define COLOR_GREEN 65280; // (0, 255, 0)
+#define COLOR_BLUE 255; // (0, 0, 255)
