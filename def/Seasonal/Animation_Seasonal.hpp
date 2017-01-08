71ae5361abe558e67c8206518bb57be5671e07a0
Mikayla Fischler <mikayla@ky8.io>
2017-01-08 11:34:14 -0500

    the winter snowing

--- def/Seasonal/Animation_Seasonal.hpp
+++ def/Seasonal/Animation_Seasonal.hpp
@@ -15,39 +15,46 @@ class Animation_Seasonal_Winter : public Animation_Seasonal {
 protected:
     Animation_Seasonal_Winter() {}
     ~Animation_Seasonal_Winter() {}
-
-    void init();
-	void snow(Adafruit_NeoPixel& strip, unsigned int i, unsigned short int left, unsigned short int right);
 };
 
-class Animation_Seasonal_Winter_Win1Snow : public Animation_Seasonal_Winter {
+class Animation_Seasonal_Winter_Snow : public Animation_Seasonal_Winter {
+protected:
+    Animation_Seasonal_Winter() {}
+    ~Animation_Seasonal_Winter() {}
+
+    void init();
+	void snow(Adafruit_NeoPixel& strip, unsigned int i, unsigned short int left_spacing, unsigned short int right_spacing);
+	void snow_step(Adafruit_NeoPixel& strip);
+};
+
+class Animation_Seasonal_Winter_Snow_Win1 : public Animation_Seasonal_Winter_Snow {
 protected:
     void init();
 public:
-    Animation_Seasonal_Winter_Win1Snow() {}
-    ~Animation_Seasonal_Winter_Win1Snow() {}
+    Animation_Seasonal_Winter_Snow_Win1() {}
+    ~Animation_Seasonal_Winter_Snow_Win1() {}
 
     void step();
 	void clean();
 };
 
-class Animation_Seasonal_Winter_Win2Snow : public Animation_Seasonal_Winter {
+class Animation_Seasonal_Winter_Snow_Win2 : public Animation_Seasonal_Winter_Snow {
 protected:
 	void init();
 public:
-	Animation_Seasonal_Winter_Win2Snow() {}
-	~Animation_Seasonal_Winter_Win2Snow() {}
+	Animation_Seasonal_Winter_Snow_Win2() {}
+	~Animation_Seasonal_Winter_Snow_Win2() {}
 
 	void step();
 	void clean();
 };
 
-class Animation_Seasonal_Winter_Win3Snow : public Animation_Seasonal_Winter {
+class Animation_Seasonal_Winter_Snow_Win3 : public Animation_Seasonal_Winter_Snow {
 protected:
 	void init();
 public:
-	Animation_Seasonal_Winter_Win3Snow() {}
-	~Animation_Seasonal_Winter_Win3Snow() {}
+	Animation_Seasonal_Winter_Snow_Win3() {}
+	~Animation_Seasonal_Winter_Snow_Win3() {}
 
 	void step();
 	void clean();
