c084e4bef9a941f7806f82724ebce331962e35ee
Mikayla Fischler <mikayla@ky8.io>
2017-01-09 21:57:15 -0500

    use header files

---
+++ def/animations.h
@@ -0,0 +1,19 @@
+#ifndef ANIMATION_IMPORT_H
+#define ANIMATION_IMPORT_H
+
+// Disable Animations
+#include "Disable/Animation_Disable.hpp"
+
+// Static Animations
+#include "Static/Animation_Static.hpp"
+
+// Simple Animations
+#include "Simple/Animation_Simple.hpp"
+
+// Seasonal Animations
+#include "Seasonal/Animation_Seasonal.hpp"
+
+// Holiday Animations
+#include "Holiday/Animation_Holiday.hpp"
+
+#endif
