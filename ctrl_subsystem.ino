395f925ff6387fb6f3ff3594801b3497e4d396da
Michael1516 <micron1314@gmail.com>
2017-01-11 11:38:34 -0500

    ctrl loop

---
+++ ctrl_subsystem.ino
@@ -0,0 +1,9 @@
+/*
+  Dorm LED Project: ctrl_subsystem.ino
+  This file manages timing and execution of control commands.
+
+  Created by: Michael Fischler
+  01/11/2017 @ WPI
+*/
+
+void ctrl_main_loop() {}
