10810832d678a470ef2a6b626b20b99616d3b8ac
Mikayla Fischler <mikayla@ky8.io>
2017-01-31 16:35:32 -0500

    color wheel helper function

--- util/led.c
+++ util/led.c
@@ -47,4 +47,22 @@ uint8_t blueFromColor(uint32_t color){
     return color & 0xFF;
 }
 
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+	WheelPos = 255 - WheelPos;
+
+	if (WheelPos < 85) {
+		return window_generic.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+	}
+
+	if (WheelPos < 170) {
+		WheelPos -= 85;
+		return window_generic.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+	}
+
+	WheelPos -= 170;
+	return window_generic.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
 #endif
