f238380ee47b4bb41baa0628d2fab0c72e3b85a0
Michael1516 <micron1314@gmail.com>
2017-03-07 17:39:18 -0500

    removed excessive basic digital/analog I/O classes
    no reason to have an entire class for them

--- conf/dev_types.h
+++ conf/dev_types.h
@@ -4,23 +4,9 @@
 // Types of Devices Present in this System
 // Type is a 16-bit unsigned int, but it is treated as an 8-bit unsigned int since that is all that is needed
 
-#define DEV_UNKNOWN_DEVICE 0xFFFFFFFF
+#define DEV_UNKNOWN_DEVICE 0x0
 
-// Digital I/O
-#define DEV_GENERIC_D_INPUT 0x00000000
-#define DEV_PUSH_BUTTON 0x00000001
-
-#define DEV_GENERIC_D_OUTPUT 0x0000000F
-#define DEV_LED 0x0000001F
-
-// Analog I/O
-#define DEV_GENERIC_A_INPUT 0x000000FF
-
-#define DEV_GENERIC_A_OUTPUT 0x00000FFF
-
-// Multi-Component Systems
-#define DEV_GENERIC_MCOMPONENT 0x00000FFFF
-#define DEV_LED_PUSH_BUTTON 0x00001FFFF
-#define DEV_MSGEQ7 0x00002FFFF
+#define DEV_LED_PUSH_BUTTON 0x1
+#define DEV_MSGEQ7 0x2
 
 #endif
--- dev/IODevice.cpp
+++
@@ -1,9 +0,0 @@
-#include "IODevice.hpp"
-
-// <<constructors>>
-IODevice::IODevice(String name, int port, unsigned int type) : Device(name, type), value(0) {
-	this->port = port;
-}
-
-// <<destructor>>
-IODevice::~IODevice() {}
