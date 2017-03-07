edeee6fd56e23e704de1ce032ba232fb21c5b709
Michael1516 <micron1314@gmail.com>
2017-03-07 17:05:09 -0500

    less code, faster

--- dev/DigitalDevice.cpp
+++ dev/DigitalDevice.cpp
@@ -11,7 +11,7 @@ DigitalDevice::~DigitalDevice() {}
 
 // write to the digital device
 void DigitalDevice::write(int value) {
-	if (this->io_type == OUTPUT && this->port >= 0) {
+	if (this->io_type == OUTPUT) {
 		digitalWrite(this->port, value);
 		this->value = value;
 	}
@@ -19,7 +19,7 @@ void DigitalDevice::write(int value) {
 
 // read this digital device
 int DigitalDevice::read() {
-	if ((this->io_type == INPUT || this->io_type == INPUT_PULLUP) && this->port >= 0) {
+	if (this->io_type == INPUT || this->io_type == INPUT_PULLUP) {
 		return digitalRead(this->port);
 	} else {
 		return this->value;
