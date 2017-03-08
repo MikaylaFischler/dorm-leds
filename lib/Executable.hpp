33bfc033d8919141bd5c80856f2902f1414fd5ec
Michael1516 <micron1314@gmail.com>
2017-03-07 22:02:31 -0500

    use step for processes, some formatting cleanup

--- lib/Executable.hpp
+++ lib/Executable.hpp
@@ -15,8 +15,9 @@ public:
 	virtual ~Executable();
 
     virtual void init() = 0;
+    virtual void step() = 0;
     virtual void clean() = 0;
-	
+
     String getName();
     unsigned long int getUpdateRate();
 };
