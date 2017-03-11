ffbd2f8d2d9d32a68db04173c9873d65838b4071
Mikayla Fischler <mikayla@ky8.io>
2017-03-11 13:42:51 -0500

    equalizer fixes

--- dev/MSGEQ7.hpp
+++ dev/MSGEQ7.hpp
@@ -17,7 +17,7 @@ private:
 
 	int spectrum_values[7];
 
-	static const int NOISE_FILTER = 0;
+	static const int NOISE_FILTER = 80;
 public:
 	MSGEQ7(String name, int strobe_port, int reset_port, int input_port);
 	virtual ~MSGEQ7();
