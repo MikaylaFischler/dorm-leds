6a467e5e8f40b87a7cc112aff6214fd1572a35b0
Michael1516 <micron1314@gmail.com>
2017-03-11 21:41:45 -0500

    math for led code

---
+++ util/ledmath.c
@@ -0,0 +1,29 @@
+#ifndef LEDMATH_C_
+#define LEDMATH_C_
+
+/*
+  Dorm LED Project: ledmath.c
+  This file contains mathematical utility functions for dorm LEDs.
+
+  Created by: Michael Fischler
+  2017-03-11 @ WPI
+
+  Calculated Brightness Curves: https://www.desmos.com/calculator/xqhb9ecn01
+*/
+
+// curve brightness input quadratically
+unsigned long int quadraticBrightness(long unsigned int x) {
+	return (unsigned long int) round(0.0039215686 * (double) pow(x, 2));
+}
+
+// curve brightness input cubically
+unsigned long int cubicBrightness(long unsigned int x) {
+	return (unsigned long int) round(0.0000153786 * (double) pow(x, 3));
+}
+
+// curve brightness input quartically
+unsigned long int quarticBrightness(long unsigned int x) {
+	return (unsigned long int) round(0.000000060308 * (double) pow(x, 4));
+}
+
+#endif
