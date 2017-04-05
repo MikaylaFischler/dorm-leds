01a26f3606623417edec2b6529ee4ea976bd1ad8
Mikayla Fischler <mikayla@ky8.io>
2017-04-05 01:45:22 -0400

    added colors

--- lib/led_color.h
+++ lib/led_color.h
@@ -1,15 +1,13 @@
 #ifndef LED_COLOR_H_
 #define LED_COLOR_H_
 
-// color definitions
+/* ~~~ color definitions ~~~ */
+
+// basic colors
 #define COLOR_OFF 0 			// (0, 0, 0)
 
 #define COLOR_BLACK 0 			// (0, 0, 0)
 #define COLOR_WHITE 16777215 	// (255, 255, 255)
-
-#define COLOR_DIM_OFF_WHITE 4926745 // (75, 45, 25)
-
-#define COLOR_LIGHT_SILVER 11120823 // (169, 176, 183)
 
 #define COLOR_RED 16711680 				// (255, 0, 0)
 #define COLOR_MAROON 16717083 			// (255, 21, 27)
@@ -21,5 +19,15 @@
 #define COLOR_BLUE 255 					// (0, 0, 255)
 #define COLOR_INDIGO 6553855			// (100, 0, 255)
 #define COLOR_VIOLET 16711935 			// (255, 0, 255)
+
+// specific colors
+
+// whites
+#define COLOR_DIM_WHITE 6579300		// (100, 100, 100)
+#define COLOR_DIM_OFF_WHITE 4926745 // (75, 45, 25)
+#define COLOR_LIGHT_SILVER 11120823 // (169, 176, 183)
+
+// blues
+#define COLOR_RAINY_BLUE 987135		// (15, 15, 255)
 
 #endif
