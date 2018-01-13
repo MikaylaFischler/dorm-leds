26cd10f2997df5297e92fa816a25937e04b609f0
MikaylaFischler <micron1314@gmail.com>
2018-01-13 01:03:01 -0500

    doxygen comments

--- util/ledmath.c
+++ util/ledmath.c
@@ -1,40 +1,56 @@
 #ifndef LEDMATH_C_
 #define LEDMATH_C_
 
-/*
-  Dorm LED Project: ledmath.c
-  This file contains mathematical utility functions for dorm LEDs.
-
-  Created by: Mikayla Fischler
-  2017-03-11 @ WPI
-
-  Calculated Brightness Curves: https://www.desmos.com/calculator/evodsxrtjj
+/*!
+	@file ledmath.c
+  	@brief This file contains mathematical utility functions for dorm LEDs.<br/>
+	<a target="_blank" href="https://www.desmos.com/calculator/evodsxrtjj">Calculated Brightness Curves</a>
 */
 
 /* ~~~ Brightness Curves ~~~ */
 /* Curve input to bias towards lower outputs to cause more noticeable changes in brightness */
 
-// curve brightness input quadratically
+/*!
+	@brief Curve brightness input quadratically
+	@param x input
+	@return function output
+*/
 unsigned long int quadraticBrightness(long unsigned int x) {
 	return (unsigned long int) round(0.003921568627 * (double) pow(x, 2));
 }
 
-// curve brightness input cubically
+/*!
+	@brief Curve brightness input cubically
+	@param x input
+	@return function output
+*/
 unsigned long int cubicBrightness(long unsigned int x) {
 	return (unsigned long int) round(0.0000153787005 * (double) pow(x, 3));
 }
 
-// curve brightness input quartically
+/*!
+	@brief Curve brightness input quartically
+	@param x input
+	@return function output
+*/
 unsigned long int quarticBrightness(long unsigned int x) {
 	return (unsigned long int) round(0.0000000603086294 * (double) pow(x, 4));
 }
 
-// curve inverse 4.10612163x^{0.745}
+/*!
+	@brief Curve brightness input by inverse 4.10612163x^{0.745}
+	@param x input
+	@return function output
+*/
 unsigned long int inverseFractional_0745_Brightness(long unsigned int x) {
 	return (unsigned long int) round(4.1083529414 * (double) pow(x, 0.745));
 }
 
-// curve inverse 12.444452145x^{0.545}
+/*!
+	@brief Curve brightness input by inverse 12.444452145x^{0.545}
+	@param x input
+	@return function output
+*/
 unsigned long int inverseFractional_0545_Brightness(long unsigned int x) {
 	return (unsigned long int) round(12.444452148 * (double) pow(x, 0.545));
 }
